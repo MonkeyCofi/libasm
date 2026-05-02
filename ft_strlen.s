8ea18f065122ea66349ca7a3870f4982fedab553
MonkeyCofi <piercediet06@gmail.com>
2026-05-02 22:08:30 +0400

    almost finished with strlen

--- ft_strlen.s
+++ ft_strlen.s
@@ -1,1 +1,23 @@
-; store the strlen in EAX register
+; size_t strlen(const char *str) -> function signature
+
+section .text:
+  global ft_strlen
+; rax register will be used to store the value
+ft_strlen:
+  xor rax, rax  ; stores 0 into the rax register
+
+loop:
+  ; compare the current byte with 0
+  ; the string parameter is stored in the rdi register
+  ; the length of the string is stored in the rax register
+  ; therefore [rdi + rax] will be the currnet character of the string
+  cmp [rdi + rax], 0  ; cmp [rdi + rax] means 
+  ; if the characters are equal, jump to return
+  ; else if the characters are not equal, continue the loop
+  je .return
+  inc rax
+  jmp loop
+
+return:
+  ret
+
--- hello.s
+++ hello.s
@@ -1,11 +1,16 @@
 section .data
-  msg db "hello, world", 10
-  len $ - msg
+  message db "hello, world", 10
+  len equ $ - message
 
 section .text
   global _start
+
 _start:
-  mov rax, 1  ; syscall for write 
-  mov rdx, 1  ; stdout fd to write
-  mov rdi, msg
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, message
+  mov rdx, len
+  syscall
+  mov eax, 60
+  mov rdi, 0
   syscall
