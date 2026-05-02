5f621f32e121fbf3351cd2ff08ebe6c7c005e6ca
MonkeyCofi <piercediet06@gmail.com>
2026-05-02 22:14:35 +0400

    created a makefile, and added a ft_strlen test in the main file

--- ft_strlen.s
+++ ft_strlen.s
@@ -1,23 +1,23 @@
 ; size_t strlen(const char *str) -> function signature
 
-section .text:
+section .text
   global ft_strlen
 ; rax register will be used to store the value
 ft_strlen:
   xor rax, rax  ; stores 0 into the rax register
 
-loop:
+.loop:
   ; compare the current byte with 0
   ; the string parameter is stored in the rdi register
   ; the length of the string is stored in the rax register
   ; therefore [rdi + rax] will be the currnet character of the string
-  cmp [rdi + rax], 0  ; cmp [rdi + rax] means 
+  cmp byte [rdi + rax], 0  ; cmp [rdi + rax] means 
   ; if the characters are equal, jump to return
   ; else if the characters are not equal, continue the loop
   je .return  ; jump to return if the cmp == true
   inc rax     ; increment length
-  jmp loop    ; restart the loop
+  jmp .loop    ; restart the loop
 
-return:
+.return:
   ret
 
--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern size_t ft_strlen(char *str);
+extern size_t ft_strlen(const char *str);
 
 int main(void)
 {
-
+  printf("length: %ld\n", ft_strlen("hello, world"));
 
 }
