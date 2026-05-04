222766832d75c9ab0511c3101ce7338ec77fd72a
MonkeyCofi <piercediet06@gmail.com>
2026-05-04 13:03:10 +0400

    made changes to ft_strcmp

--- Makefile
+++ Makefile
@@ -1,3 +1,22 @@
+NAME := libasm
+
 CC := gcc
 
 CFLAGS := -Wall -Wextra -Werror
+
+# convert the .s files into object files
+SRCS := ft_strlen.s ft_strcmp.s
+
+OBJS := $(SRCS:.s=.o)
+
+all: $(NAME)
+
+# recipe for every .s file
+%.o: %.s
+	nasm -f elf64 $^
+
+$(NAME): $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) main.c -o test
+
+clean:
+	rm -rf $(OBJS)
--- main.c
+++ main.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern size_t ft_strlen(const char *str);
+extern size_t	ft_strlen(const char *str);
+extern int	ft_strcmp(const char *s1, const char *s2);
+
+//int main(void)
+//{
+//	printf("ft_strlen: \n");
+//	const char *str = "hello, world!";
+//	printf("Length of string \"%s\": %ld\n", str, ft_strlen(str));
+//	printf("ft_strcmp: \n");
+//	const char *s1 = "";
+//	const char *s2 = "hello";
+//	printf("s1: \"%s\" s2: \"%s\": %d\n", s1, s2, ft_strcmp(s1, s2));
+//}
 
 int main(void)
 {
-  printf("length: %ld\n", ft_strlen("hello, world"));
-
+	const char *s1 = NULL; 
+	const char *s2 = "";
+	int diff = ft_strcmp(s1, s2);
+	printf("difference between '%s' and '%s': %d\n", s1, s2, diff);
 }
