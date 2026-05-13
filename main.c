1f91a60ec0483dfdc2fbca6406c0cb5d6e1bc377
MonkeyCofi <piercediet06@gmail.com>
2026-05-13 20:33:58 +0400

    visually separated the tests in main

--- main.c
+++ main.c
@@ -13,15 +13,39 @@ extern char*	ft_strcpy(char *dest, char *src);
 
 int main(void)
 {
-	char *src = "hello";
-	char dest[] = "there";
-	printf("source string: %s\n\n", src);
-	printf("dest before copy: %s\n", dest);
-	char *ret = ft_strcpy(dest, src);
-	printf("dest after copy: %s\n", dest);
-	if (ret == dest)
-		printf("dest and strcpy return are the same address\n");
-	else
-		printf("strcpy returned a different address\n");
-	printf("dest: %p, strcpy: %p\n", dest, ret);
+	printf("\n");
+	/*
+		tests for ft_strlen
+	*/
+	{
+		printf("=======================\n");
+		printf("ft_strlen tests\n");
+		printf("=======================\n");
+		char *str = "hello, world";
+		ssize_t len = ft_strlen(str);
+		printf("the length of str %s is %ld\n", str, len);
+		str = "";
+		len = ft_strlen(str);
+		printf("the length of str %s is %ld\n", str, len);
+		printf("=======================\n\n");
+	}
+
+	/*
+		tests for ft_strcpy
+	*/
+	{
+		printf("ft_strcpy tests\n");
+		char *src = "hello";
+		char dest[] = "there";
+		printf("source string: %s\n\n", src);
+		printf("dest before copy: %s\n", dest);
+		char *ret = ft_strcpy(dest, src);
+		printf("dest after copy: %s\n", dest);
+		if (ret == dest)
+			printf("dest and strcpy return are the same address\n");
+		else
+			printf("strcpy returned a different address\n");
+		printf("dest: %p, strcpy: %p\n", dest, ret);
+		printf("=======================\n\n");
+	}
 }
