9442b0cb7d517d729f7a2d39260e38dd56f3410b
MonkeyCofi <piercediet06@gmail.com>
2026-05-14 16:15:26 +0400

    fixed ft_strdup when compiled with fsanitize=address

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -5,18 +5,20 @@ section .text
 	global ft_strdup
 
 ft_strdup:
+	sub rsp, 8
+	mov [rsp], rdi
 	extern ft_strlen
 	call ft_strlen
-	mov r8, rdi		; save the string into the r8 register
 	mov edi, eax	; edi will have the length
 	inc edi
 	extern malloc
 	call malloc WRT ..plt
 					; rax will now have the malloc'd address
 	mov rdi, rax	; store the malloc'd address into rdi as strcpy dest
-	mov rsi, r8		; move the str from rcx into rsi as strcpy src
+	mov rsi, [rsp]		; move the str from rcx into rsi as strcpy src
 	extern ft_strcpy
 	call ft_strcpy
+	add rsp, 8
 	jmp _return
 
 _return:
