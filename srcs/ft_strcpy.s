5624912eec9034085ed83e31ed4f5166673c87a9
MonkeyCofi <piercediet06@gmail.com>
2026-05-16 19:36:05 +0400

    fixed a bug in strcpy

--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -4,10 +4,14 @@
 
 section .text
 	global ft_strcpy
-	xor rax, rax
-	mov rax, [rdi]
 
 ft_strcpy:
+	xor rax, rax
+	; mov rax, rdi
+	lea rax, [rdi]
+	jmp _loop
+
+_loop:
 	cmp [rsi], BYTE 0	; compare the current $rsi character to nul
 	je _return			; if it is equal, jump to return
 	mov cl, BYTE [rsi]	; move the character to the cl register 
